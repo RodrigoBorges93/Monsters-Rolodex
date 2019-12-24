bc466707d4d281ea7a6fc505a30928d718fec644
Rodrigo dos Santos Borges <rodrigoborges93@gmail.com>
2019-12-24 17:23:13 -0300

    deploy

---
+++ build/precache-manifest.dad5834b8f3858c8b06e34b2e76488f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d80c752b4ed78c9b824c",
+    "url": "/Monsters-Rolodex/static/css/main.4a2dd9d7.chunk.css"
+  },
+  {
+    "revision": "d80c752b4ed78c9b824c",
+    "url": "/Monsters-Rolodex/static/js/main.1a5ebbc1.chunk.js"
+  },
+  {
+    "revision": "3c42c109e430c626051d",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.2f1dcf62.js"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/Monsters-Rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "3ffaaf96f363bec4a8ea7469b0708277",
+    "url": "/Monsters-Rolodex/index.html"
+  }
+];
